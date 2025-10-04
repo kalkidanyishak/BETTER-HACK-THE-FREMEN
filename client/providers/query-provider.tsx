449131efc82503253b107d93101ac86e98ec4f2f
kalkidanyishak <kalkidanyishak001@gmail.com>
2025-10-04 18:38:43 +0300

    installed them  provider and toaster

---
+++ client/providers/query-provider.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+export default function AppProvider({ children }: AppProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class">
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
