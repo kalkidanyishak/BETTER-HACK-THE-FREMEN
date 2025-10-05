129ed1fcd37183090da72f75640265bcd09f72b2
kalkidanyishak <kalkidanyishak001@gmail.com>
2025-10-05 08:13:09 +0300

    fixing express

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 // src/core/server.ts
-import express from "express";
+import express  { Application, Request, Response } from "express";
 import cors from "cors";
 import { endpoint } from "./lib/endpoints";
 
@@ -35,8 +35,8 @@ app.use("/api/interactions", endpoint("interaction"));
 
 app.get("/", (_req, res) => res.json({ message: "AutoERP API running" }));
 
-app.get('/health', (req: Request, res: Response) => { // <-- TS thinks 'Response' is the built-in web type
-  res.status(200).send('OK'); // ERROR: .status is a number here, not a function
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).send('OK');
 });
 
 app.listen(PORT, () => {
